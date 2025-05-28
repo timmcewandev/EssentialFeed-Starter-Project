bb7e612637616961f77d666eb708bd39f6a149c4
Tim McEwan <timmcewandev@gmail.com>
2025-05-28 14:02:21 -0600

    Move HTTPCLient logic to testable HTTPClientApy

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTest.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTest.swift
@@ -9,25 +9,35 @@ import XCTest
 
 class RemoteFeedLoader {
     func load() {
-        HTTPClient.shared.requestedURL = URL(string: "http://a-url.com")
+        HTTPClient.shared.get(from: URL(string: "http://a-url.com")!)
     }
 }
 class HTTPClient {
-    static let shared = HTTPClient()
-    private init() {}
+    // Now Considred global state for Singleton
+    static var shared = HTTPClient()
+    
+    func get(from url: URL) {}
+}
+
+class HTTPClientSpy: HTTPClient {
+    override func get(from url: URL) {
+        requestedURL = url
+    }
     var requestedURL: URL?
 }
 
 final class RemoteFeedLoaderTest: XCTestCase {
     
     func test_init_DoesNotRequestDataFromURL() {
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         let _ = RemoteFeedLoader()
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_DoesRequestDataFromURL() {
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         let sut = RemoteFeedLoader()
         sut.load()
         
